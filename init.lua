cd516c39ee4c2485936fd20467da94f9c7d8611a
Jorge Galindo <jorge.galindo@wizeline.com>
2023-04-17 11:05:15 -0600

    base set up

---
+++ init.lua
@@ -0,0 +1,1 @@
+require("georgexx009")
---
+++ lua/georgexx009/packer.lua
@@ -0,0 +1,28 @@
+vim.cmd.packadd("packer.nvim")
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- lps-zero
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+  
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  }
+end)
